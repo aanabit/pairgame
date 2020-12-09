2a6888b689aa25a93d5573e9a1d6abd6b4e4afc8
Amaia Anabitarte <amaia@moodle.com>
2020-12-09 11:12:45 +0100

    Initial commit

---
+++ H5P.PairGame-1.0/timer.js
@@ -0,0 +1,55 @@
+(function (PairGame, Timer) {
+
+  /**
+   * Adapter between memory game and H5P.Timer
+   *
+   * @class H5P.PairGame.Timer
+   * @extends H5P.Timer
+   * @param {Element} element
+   */
+  PairGame.Timer = function (element) {
+    /** @alias H5P.PairGame.Timer# */
+    var self = this;
+
+    // Initialize event inheritance
+    Timer.call(self, 100);
+
+    /** @private {string} */
+    var naturalState = element.innerText;
+
+    /**
+     * Set up callback for time updates.
+     * Formats time stamp for humans.
+     *
+     * @private
+     */
+    var update = function () {
+      var time = self.getTime();
+
+      var minutes = Timer.extractTimeElement(time, 'minutes');
+      var seconds = Timer.extractTimeElement(time, 'seconds') % 60;
+
+      // Update duration attribute
+      element.setAttribute('datetime', 'PT' + minutes + 'M' + seconds + 'S');
+
+      // Add leading zero
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+
+      element.innerText = minutes + ':' + seconds;
+    };
+
+    // Setup default behavior
+    self.notify('every_tenth_second', update);
+    self.on('reset', function () {
+      element.innerText = naturalState;
+      self.notify('every_tenth_second', update);
+    });
+  };
+
+  // Inheritance
+  PairGame.Timer.prototype = Object.create(Timer.prototype);
+  PairGame.Timer.prototype.constructor = PairGame.Timer;
+
+})(H5P.PairGame, H5P.Timer);
